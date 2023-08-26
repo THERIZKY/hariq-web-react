243d71be474d421eed0bd6baab350060a94f9e4a
RIZU <68154002+THERIZKY@users.noreply.github.com>
2023-08-26 09:35:58 +0700

    Changing Default title and Description

--- src/Pages/Rules.jsx
+++ src/Pages/Rules.jsx
@@ -5,7 +5,12 @@ import Navbar from "../Component/Navbar";
 const Rules = () => {
 	useEffect(() => {
 		document.title = "Hariq Rules || Hariq MC";
-		document.description = "Rules And Term In Hariq MC";
+		const MetaDataDesc = document.querySelector('meta[name="description"]');
+
+		MetaDataDesc.setAttribute(
+			"content",
+			"Rules And Terms In Hariq Mc Is All Here",
+		);
 	}, []);
 
 	return (
@@ -13,9 +18,9 @@ const Rules = () => {
 			<Navbar />
 			<div className="container">
 				<div className="header-rules animate__animated animate__fadeInDown">
-					<i class="fa-solid fa-triangle-exclamation fa-2xl"></i>
+					<i className="fa-solid fa-triangle-exclamation fa-2xl"></i>
 					<h1>Rules!</h1>
-					<i class="fa-solid fa-triangle-exclamation fa-2xl"></i>
+					<i className="fa-solid fa-triangle-exclamation fa-2xl"></i>
 				</div>
 				<div className="rules-container animate__animated animate__fadeInUp">
 					<RulesContent />
